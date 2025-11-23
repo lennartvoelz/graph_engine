8b7119845400b5319fe2ac7cd2a0538b1ab585a4
lennartvoelz <lennart.voelz@tu-dortmund.de>
2025-11-23 01:08:44 +0100

    chore: update cmake toolchain to include new tests

--- tests/Tensor/TestStorage.cpp
+++ tests/Tensor/TestStorage.cpp
@@ -4,7 +4,6 @@
 #include <GraphEngine/Tensor/Storage.hpp>
 #include <vector>
 
-using namespace GraphEngine;
 namespace gTest = ::testing;
 using gTest::NotNull;
 using StorageTypes = gTest::Types<float, double, int>;  // Typed Tests
@@ -17,53 +16,64 @@ public:
 
 protected:
 	auto SetUp() -> void override {
-		small_ = 1e2;
-		large_ = 1e6;
+		small = 1e2;
+		large = 1e6;
 	}
 
-	size_type small_;
-	size_type large_;
+	size_type small;
+	size_type large;
 };
 
 // === Basic Tests ===
 
 TYPED_TEST(TestStorage, ConstructorAllocatesSmall) {
-	const std::size_t size = this->small_;
-	Storage<TypeParam> storage(size);
+	const std::size_t size = this->small;
+
+	GraphEngine::Storage<TypeParam> storage(size);
+
 	EXPECT_EQ(storage.size(), size);
 	EXPECT_THAT(storage.data(), NotNull());
 }
 
 TYPED_TEST(TestStorage, AllocatesLargeDataModification) {
-	const std::size_t size = this->large_;
-	Storage<TypeParam> storage(size);
+	const std::size_t size = this->large;
+
+	GraphEngine::Storage<TypeParam> storage(size);
 	// Check that data modification works
 	storage.data()[10'000] = 42;
+
 	EXPECT_EQ(storage.size(), size);
 	EXPECT_THAT(storage.data(), NotNull());
 	EXPECT_EQ(storage.data()[10'000], TypeParam{42});
 }
 
 TYPED_TEST(TestStorage, DataPointerIsValid) {
-	const std::size_t size = this->small_;
-	Storage<TypeParam> storage(size);
+	const std::size_t size = this->small;
+
+	GraphEngine::Storage<TypeParam> storage(size);
 	std::vector<TypeParam> vector(size, 42);
+	// Check that copying works by checking on random indeces
 	auto* dataPtr = storage.data();
 	std::copy(vector.begin(), vector.end(), dataPtr);
+
 	EXPECT_EQ(dataPtr[0], 42);
 	EXPECT_EQ(dataPtr[10], 42);
 }
 
 TYPED_TEST(TestStorage, SizeZeroCreatesEmtpyStorage) {
 	const std::size_t size = 0;
-	Storage<TypeParam> storage(size);
+
+	GraphEngine::Storage<TypeParam> storage(size);
+
 	EXPECT_EQ(storage.size(), size);
 }
 
 // === Shared Flag Tests ===
 
 TYPED_TEST(TestStorage, IsSharedReturnsFalseByDefault) {
-	const std::size_t size = this->small_;
-	Storage<TypeParam> storage(size);
+	const std::size_t size = this->small;
+
+	GraphEngine::Storage<TypeParam> storage(size);
+
 	EXPECT_FALSE(storage.is_shared());
 }
